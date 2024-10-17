6a0872c33b6343c7107eaf56af196797226ba9cb
Ansab-Sajjad <ansab@troontechnologies.com>
2024-10-17 21:30:23 +0500

    Initial setup

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+
+  describe("Let's Automate GRT Health.", () => {
+
+  
+    before(() => {
+      cy.clearLocalStorageSnapshot();
+      cy.visit("https://grt-staging-api.grtinsight.com/en-US/", { failOnStatusCode: false }); // HHH Server
+  
+    });
+ 
+    // it("Login to GRT", () => {
+    //   cy.visit("https://grt-staging-api.grtinsight.com/en-US/");
+    // });
+ 
+it('', () => {
+    
+});
+
+it('', () => {
+    
+}); 
+})
